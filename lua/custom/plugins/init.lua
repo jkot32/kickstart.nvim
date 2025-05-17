ef50910911c83d2e8c728c815e0ab185bcbfab61
BGP2 <cooom@seznam.cz>
2025-05-17 13:25:05 +0200

    Enabled nerdfonts, added Cfg and Config commands

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.api.nvim_create_user_command('Cfg', 'e ' .. vim.fn.stdpath 'config' .. '/init.lua', {
+  desc = 'Config file location',
+})
+vim.api.nvim_create_user_command('Configs', 'echo stdpath("config")', {
+  desc = 'Config file location',
+})
+
 return {}
