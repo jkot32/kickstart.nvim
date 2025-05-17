6d9cfeab3f511ce8c141229b794226ee9f932756
BGP2 <cooom@seznam.cz>
2025-05-17 13:34:38 +0200

    Config editing and folder commands

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,10 +1,15 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-vim.api.nvim_create_user_command('Cfg', 'e ' .. vim.fn.stdpath 'config' .. '/init.lua', {
-  desc = 'Config file location',
+vim.api.nvim_create_user_command('Cfg', 'e ' .. vim.fn.stdpath 'config' .. '/lua/custom/plugins/init.lua', {
+  desc = 'Open main config file',
 })
+
+vim.api.nvim_create_user_command('Cfgs', 'e ' .. vim.fn.stdpath 'config' .. '/init.lua', {
+  desc = 'Open main config file',
+})
+
+vim.api.nvim_create_user_command('Config', 'Ex ' .. vim.fn.stdpath 'config', {
+  desc = 'Open config file location',
+})
+
 vim.api.nvim_create_user_command('Configs', 'echo stdpath("config")', {
   desc = 'Config file location',
 })
